140eddaedc5862d1406e85b965003b5b9cb63ad3
danie667 <bombuuk@gmail.com>
2024-03-20 12:50:56 +0500

    Ваше сообщение коммита здесь

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("body"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle");
+
+      modeToggle.addEventListener("click", () => {
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+        
+      });
+
+      searchToggle.addEventListener("click", () => {
+        searchToggle.classList.toggle("active");
+      });
+
+      
