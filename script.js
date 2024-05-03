e7f418f6d23a50012b05b0c22161c6716a846e0b
danie667 <bombuuk@gmail.com>
2024-05-03 21:56:54 +0500

    change for last module

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("body"),
       modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchToggle");
+      searchToggle = document.querySelector(".searchToggle"),
+      button = document.getElementById("themeButton"),
+      container = document.querySelector(".container")
 
       modeToggle.addEventListener("click", () => {
         modeToggle.classList.toggle("active");
         body.classList.toggle("dark");
-        
+        const currentTheme = container.getAttribute("data-bs-theme");
+        if (currentTheme === "light") {
+          container.setAttribute("data-bs-theme", "dark");
+      } else {
+          container.setAttribute("data-bs-theme", "light");
+      }
       });
 
       searchToggle.addEventListener("click", () => {
